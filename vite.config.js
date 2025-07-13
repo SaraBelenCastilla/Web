6b024efc72491dd4cc88e1651a720c7e3c21ecda
SaraBelenCastilla <saracastillalagata@gmail.com>
2025-07-13 19:30:54 +0200

    dcuarta

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-       outDir: 'dist',
-       chunkSizeWarningLimit: 1000
-     },
-     base: '/'
-})
+  build: {
+    outDir: 'dist', // Carpeta de salida para los archivos estáticos
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Divide las dependencias principales en un archivo separado
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Ajusta el límite de tamaño de los chunks (opcional)
+  },
+  
+  server: {
+    port: 3000,
+  },
+});
